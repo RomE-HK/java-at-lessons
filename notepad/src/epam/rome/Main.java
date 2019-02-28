97f5059525ee954491568bd776f39135dedf150f
RomE <rome-0@yandex.ru>
2019-02-28 18:52:23 +0300

    Added some border checks and small fixes

--- notepad/src/epam/rome/Main.java
+++ notepad/src/epam/rome/Main.java
@@ -6,19 +6,20 @@ public class Main {
         Notepad pad = new Notepad();
 
         NotepadRecord note0 = new NotepadRecord("Zoglavie", "Test your luck!");
-        pad.addNote(note0);
         NotepadRecord note1 = new NotepadRecord("Zaglavie", "Test your might!");
-        pad.addNote(note1);
         NotepadRecord note2 = new NotepadRecord("Zagolovock", "Test your strength!");
-        pad.addNote(note2);
+        for (int i = 0; i < 50; i++) {
+            pad.addNote(note0);
+            pad.addNote(note1);
+            pad.addNote(note2);
+        }
+        pad.addNote(null);
+
+        pad.delNote(149);
+        pad.delNote(22);
+        pad.delNote(-1);
 
         pad.showAllNotes();
         System.out.println(pad.numberOfNotes());
-
-        /* for (int i = 0; i < pad.numberOfNotes(); i++) {
-            pad.getNote(i).dateUpdate();
-        }
-        pad.showAllNotes();
-        System.out.println(pad.numberOfNotes()); */
     }
 }
--- notepad/src/epam/rome/Notepad.java
+++ notepad/src/epam/rome/Notepad.java
@@ -1,5 +1,7 @@
 package epam.rome;
 
+import java.util.Arrays;
+
 public class Notepad {
     private static final int MIN_ARRAY_LENGTH = 32;
     private int notesCount;
@@ -10,35 +12,51 @@ public class Notepad {
         allNotes = new NotepadRecord[MIN_ARRAY_LENGTH];
     }
 
-    public NotepadRecord getNote(int noteNumber) {
-        return allNotes[noteNumber];
+    // Нужен ли этот геттер? -_-
+    public NotepadRecord getNote(int noteIndex) {
+        if ((noteIndex >= 0) && (noteIndex < notesCount)) {
+            return allNotes[noteIndex];
+        } else
+            return null;
     }
 
     // Пока только набросок
     public void addNote(NotepadRecord note) {
-        allNotes[notesCount] = note;
-        notesCount++;
-    }
-
-    // В процессе написания реализации
-    public void deleteNote(int noteNumber) {
-        if (noteNumber >= 0 && noteNumber < notesCount) {
-            // allNotes[noteNumber] = allNotes[notesCount - 1];
-            // allNotes[notesCount - 1] = null;
-            allNotes[noteNumber].replaceRecord(allNotes[notesCount - 1]);
-            allNotes[notesCount - 1].replaceRecord(null);
-            notesCount--;
-            this.resizeArray();
+        if (note instanceof NotepadRecord) {
+            allNotes[notesCount] = note;
+            notesCount++;
+            resizeArray();
+        } else {
+            System.out.println("Wrong input type of data");
         }
     }
 
-    public void showNote(int noteNumber) {
-        allNotes[noteNumber].showRecord();
+    public void delNote(int noteIndex) {
+        if ((noteIndex >= 0) && (noteIndex < notesCount)) {
+            if (noteIndex < notesCount - 1) {
+                allNotes[noteIndex] = allNotes[notesCount - 1];
+            }
+            allNotes[notesCount - 1] = null;
+            // allNotes[noteIndex].replaceRecord(allNotes[notesCount - 1]);
+            // allNotes[notesCount - 1].replaceRecord(null);
+            notesCount--;
+            resizeArray();
+            System.out.println("Note deleted");
+        } else {
+            System.out.println("Wrong note index");
+        }
+    }
+
+    public void showNote(int noteIndex) {
+        if ((noteIndex >= 0) && (noteIndex < notesCount)) {
+            allNotes[noteIndex].showRecord();
+        }
     }
 
     public void showAllNotes() {
         for (int i = 0; i < notesCount; i++) {
-            System.out.println(i);
+            System.out.println(i + " pos in allNotes");
+            System.out.println(i + 1 + " ordinal number");
             allNotes[i].showRecord();
         }
     }
@@ -47,11 +65,25 @@ public class Notepad {
         return notesCount;
     }
 
-    // Пока только набросок реализации
     private void resizeArray() {
-        if ((allNotes.length > MIN_ARRAY_LENGTH) && (notesCount < allNotes.length / 2)) {
-            //int newArraySize = allNotes.length / 32 * 16 + allNotes.length;
-            //System.arraycopy(src, srcPos, dest, destPos, length);
+        int newArraySize;
+        if (notesCount == allNotes.length) {
+            newArraySize = allNotes.length / MIN_ARRAY_LENGTH * (MIN_ARRAY_LENGTH / 2) + allNotes.length;
+            // Сделать нормальный коэффицент для увеличения массива
+
+        } else if ((notesCount > MIN_ARRAY_LENGTH) && (notesCount < allNotes.length / 2)) {
+            newArraySize = allNotes.length / 4 * 3; // Отрубаем у массива четверть, если используется меньше половины
+        } else if ((notesCount < MIN_ARRAY_LENGTH) && (notesCount < allNotes.length / 2)) {
+            newArraySize = MIN_ARRAY_LENGTH; // Сжатие массива до минимального размера
+        } else {
+            System.out.println("Resize doesn't need");
+            return;
         }
+
+        // Поправить вызов этого куска
+        NotepadRecord[] newArray = Arrays.copyOf(allNotes, newArraySize);
+        allNotes = newArray;
+        System.out.println("Resize completed succesfully");
+        return;
     }
 }
