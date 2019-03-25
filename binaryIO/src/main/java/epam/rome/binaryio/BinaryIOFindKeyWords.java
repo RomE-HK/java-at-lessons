221091fda9cda2037771a8a0a075523422c505d9
RomE <rome-0@yandex.ru>
2019-03-25 16:13:22 +0300

    Added HashMap for keywords

--- binaryIO/src/main/java/epam/rome/binaryio/BinaryIOFindKeyWords.java
+++ binaryIO/src/main/java/epam/rome/binaryio/BinaryIOFindKeyWords.java
@@ -1,47 +1,51 @@
 package epam.rome.binaryio;
 
 import java.io.BufferedReader;
-import java.io.FileOutputStream;
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Map;
+import java.util.HashMap;
 
 public class BinaryIOFindKeyWords {
 
     public static void readFile() {
-        try (FileInputStream fis = new FileInputStream("str.java")) {
+        String filePath = "binaryIO/src/main/java/epam/rome/binaryio/reading.class";
+        try (FileInputStream fis = new FileInputStream(filePath)) {
             int i;
             while ((i = fis.read()) != -1) {
-                System.out.println((char)i);
+                System.out.println((char) i);
             }
-        }
-        catch(IOException ex){
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
     public static void writeFile() {
-        try(FileOutputStream fos=new FileOutputStream("str.java")) {
+        String filePath = "binaryIO/src/main/java/epam/rome/binaryio/str.java";
+        try (FileOutputStream fos = new FileOutputStream(filePath)) {
             String text = "Hello world!";
             byte[] buffer = text.getBytes();
             fos.write(buffer, 0, buffer.length);
-        }
-        catch(IOException ex){
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
         }
     }
 
-    public static void pullKeyWords() {
-        try{
-            FileInputStream fstream = new FileInputStream("keywords.txt");
-            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+    public static Map<String, Integer> pullKeyWords() {
+        String filePath = "binaryIO/src/main/java/epam/rome/binaryio/keywords.txt";
+        Map<String, Integer> map = new HashMap<String, Integer>();
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(filePath)))) {
             String strLine;
-            while ((strLine = br.readLine()) != null){
-               System.out.println(strLine);
+            while ((strLine = br.readLine()) != null) {
+                map.put(strLine, 0);
+                System.out.println(strLine);
             }
-            br.close();
-         }catch (IOException ex){
+        } catch (IOException ex) {
             System.out.println(ex.getMessage());
-         }
+        }
+        return map;
     }
 }
--- binaryIO/src/main/java/epam/rome/binaryio/Main.java
+++ binaryIO/src/main/java/epam/rome/binaryio/Main.java
@@ -1,11 +1,15 @@
 package epam.rome.binaryio;
 
+import java.util.Map;
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
-	BinaryIOFindKeyWords.writeFile();
-    BinaryIOFindKeyWords.readFile();
-    BinaryIOFindKeyWords.pullKeyWords();
-    return;
+        Map<String, Integer> keywordsAmout = new HashMap<String, Integer>();
+        BinaryIOFindKeyWords.writeFile();
+        BinaryIOFindKeyWords.readFile();
+        keywordsAmout = BinaryIOFindKeyWords.pullKeyWords();
+        return;
     }
 }
