54fa21027e4b6026c9e89859f72bd98d7718584b
RomE <rome-0@yandex.ru>
2019-04-03 15:44:36 +0300

    All methods done

--- stringIO/src/main/java/epam/rome/stringio/Main.java
+++ stringIO/src/main/java/epam/rome/stringio/Main.java
@@ -1,8 +1,19 @@
-package epam.rome.binarystring;
+package epam.rome.stringio;
+
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        String keywordsFilePath = "src/main/java/epam/rome/byteio/keywords.txt";
+        String readingFilePath = "src/main/java/epam/rome/byteio/reading.class";
+        String writingFilePath = "src/main/java/epam/rome/byteio/result.txt";
+
+        Map<String, Integer> keywordsMap = StringIOFindKeyWords.getKeyWordsFromFile(keywordsFilePath);
+        StringIOFindKeyWords.findMatchesInFile(keywordsMap, readingFilePath);
+        StringIOFindKeyWords.writeFile(keywordsMap, writingFilePath);
+        StringIOFindKeyWords.printAllMap(keywordsMap);
+
+        return;
     }
 }
